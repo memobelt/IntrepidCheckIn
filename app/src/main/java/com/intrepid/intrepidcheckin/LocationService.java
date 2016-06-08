f7e87ce31c650e071f30a272cc98df6a41f71f5b
Memo <memobetl@gmail.com>
2016-06-08 16:59:34 -0400

    Changed variable names

--- app/src/main/java/com/intrepid/intrepidcheckin/LocationService.java
+++ app/src/main/java/com/intrepid/intrepidcheckin/LocationService.java
@@ -12,7 +12,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.NotificationCompat;
-import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -20,7 +19,7 @@ import com.google.android.gms.location.LocationServices;
 
 public class LocationService extends Service implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
-    private GoogleApiClient mGoogleApiClient;
+    private GoogleApiClient googleApiClient;
     private Location workLocation;
     public static final int POST = 0;
     public static final int CANCEL = 1;
@@ -36,13 +35,13 @@ public class LocationService extends Service implements GoogleApiClient.Connecti
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if(mGoogleApiClient==null){
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
-            mGoogleApiClient.connect();
+            googleApiClient.connect();
             workLocation = new Location("Work");
             workLocation.setLatitude(42.367053); //Intrepid Lat
             workLocation.setLongitude(-71.080161); //Intrepid Long
@@ -51,22 +50,22 @@ public class LocationService extends Service implements GoogleApiClient.Connecti
         return super.onStartCommand(intent, flags, startId);
     }
 
-    public void checkLocation(String name){
+    public void checkLocation(String name) {
         //Permission check
         if (!(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
                         != PackageManager.PERMISSION_GRANTED)) {
 
-            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
 
-            if(location != null){
-                if(location.distanceTo(workLocation)<=1000){
+            if (location != null) {
+                if (location.distanceTo(workLocation) <= 1000) {
 
                     Intent slackIntent = new Intent(getApplicationContext(), SlackMessage.class);
                     slackIntent.putExtra(Constants.NAME, name);
 
-                    android.support.v4.app.NotificationCompat.Builder mBuilder =
+                    android.support.v4.app.NotificationCompat.Builder builder =
                             new NotificationCompat.Builder(this)
                                     .setSmallIcon(R.mipmap.ic_launcher)
                                     .setContentTitle(getString(R.string.app_name))
@@ -77,24 +76,24 @@ public class LocationService extends Service implements GoogleApiClient.Connecti
                     PendingIntent slackPendingIntent =
                             PendingIntent.getService(getApplicationContext(), POST, slackIntent,
                                     PendingIntent.FLAG_UPDATE_CURRENT);
-                    mBuilder.addAction(R.drawable.post,getString(R.string.post), slackPendingIntent);
+                    builder.addAction(R.drawable.post, getString(R.string.post), slackPendingIntent);
 
                     slackIntent.putExtra(Constants.FLAG, CANCEL);
                     slackPendingIntent =
                             PendingIntent.getService(getApplicationContext(), CANCEL, slackIntent,
                                     PendingIntent.FLAG_UPDATE_CURRENT);
-                    mBuilder.addAction(R.drawable.cancel,getString(R.string.cancel), slackPendingIntent);
+                    builder.addAction(R.drawable.cancel, getString(R.string.cancel), slackPendingIntent);
 
                     slackIntent.putExtra(Constants.FLAG, STOP);
                     slackPendingIntent =
                             PendingIntent.getService(getApplicationContext(), STOP, slackIntent,
                                     PendingIntent.FLAG_UPDATE_CURRENT);
-                    mBuilder.addAction(R.drawable.stop,getString(R.string.stop), slackPendingIntent);
+                    builder.addAction(R.drawable.stop, getString(R.string.stop), slackPendingIntent);
 
-                    int mNotificationId = 001;
-                    NotificationManager mNotifyMgr =
+                    int notificationId = 001;
+                    NotificationManager notifyMgr =
                             (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                    mNotifyMgr.notify(mNotificationId, mBuilder.build());
+                    notifyMgr.notify(notificationId, builder.build());
 
                 }
             }
@@ -107,8 +106,8 @@ public class LocationService extends Service implements GoogleApiClient.Connecti
 
     @Override
     public void onConnectionSuspended(int i) {
-        if(mGoogleApiClient != null) {
-            mGoogleApiClient.disconnect();
+        if (googleApiClient != null) {
+            googleApiClient.disconnect();
         }
     }
 
@@ -120,8 +119,8 @@ public class LocationService extends Service implements GoogleApiClient.Connecti
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if(mGoogleApiClient != null) {
-            mGoogleApiClient.disconnect();
+        if (googleApiClient != null) {
+            googleApiClient.disconnect();
         }
     }
 
